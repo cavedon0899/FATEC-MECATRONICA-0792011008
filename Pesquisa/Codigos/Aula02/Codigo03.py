dadb3d4f6e3a6e104e049194316b3059b4f1cf63
cavedon0899 <62109242+cavedon0899@users.noreply.github.com>
2021-08-11 19:49:16 -0300

    Create Codigo03.py

---
+++ Pesquisa/Codigos/Aula02/Codigo03.py
@@ -0,0 +1,10 @@
+
+ def ExibirFuncionario(nome, cpf, horas_trabalhadas, salario): 
+     print('O trabalhador {0}, CPF: {1}, trabalhada {2} horas e recebe {3}R$'. format(nome, cpf, horas_trabalhadas, salario)) 
+     
+
+ExibirFuncionario('Gustavo', '499.678.268-43',  8, 1699.99)
+
+ExibirFuncionario('Guilherme', '38.504.006-4',  6, 2356.6)
+
+ExibirFuncionario('Thais', '503.602.096-2',  10, 10699.99)
